a5d43ed2d50f3380a1c05dad156db3fc00fe6024
Jen Fong-Adwent <jen@ednapiranha.com>
2013-07-16 21:37:53 -0400

    added stub

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+var level = require('level');
+var through = require('through');
+var Sublevel = require('level-sublevel');
+
+var Parallax = function (options) {
+  var self = this;
+
+  if (!options) {
+    options = {};
+  }
+
+  this.dbPath = options.db || './db';
+  this.limit = 10;
+  this.db = Sublevel(level(this.dbPath, {
+    createIfMissing: true,
+    keyEncoding: 'binary',
+    valueEncoding: 'json'
+  }));
+
+  console.log(this.db);
+};
+
+module.exports = Parallax;
--- test/test.main.js
+++ test/test.main.js
@@ -3,3 +3,8 @@
 process.env.NODE_ENV = 'test';
 
 var should = require('should');
+var Parallax = require('../main');
+
+var p = new Parallax({
+  db: './test/db'
+});
